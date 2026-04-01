fbcf7908afcca71a93f7c773372c58b4cc0943f9
Brendon W. <bwherry@midland.k12.ia.us>
2026-04-01 12:30:24 -0500

    Refactor ShoppingCart to use dependency injection

--- ShoppingCart.cs
+++ ShoppingCart.cs
@@ -1,13 +1,19 @@
 namespace knightmoves;
 
-public class ShoppingCart : Logger{
-    public List<string> items {get;}
+public class ShoppingCart
+{
+    private readonly Logger _logger;
+    public List<string> Items { get; }
 
-    public ShoppingCart(){
-        items = new List<string>();
+    public ShoppingCart(Logger logger)
+    {
+        _logger = logger;
+        Items = new List<string>();
     }
-    public void AddItem(string item){
-        items.Add(item);
-        Log($"Added item '{item}' to cart");
+
+    public void AddItem(string item)
+    {
+        Items.Add(item);
+        _logger.Log($"Added item '{item}' to cart");
     }
 }
